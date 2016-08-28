b9991a4643a3f5a832d43974dc5307bb646c3c70
shubhampatil-webonise <shubham.patil@weboniselab.com>
2016-08-29 04:04:39 +0530

    fix bugs and refactor

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :branch, 'master'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "$HOME/Deployement/#{fetch(:application)}"
+set :deploy_to, "$HOME/Deployment/#{fetch(:application)}"
 
 # Default value for :scm is :git
 set :scm, :git
